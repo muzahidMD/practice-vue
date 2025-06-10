d78d28b80f446a4061df4a45ae440a2424e2e7a1
MD Muzahidul Islam <muzahidul@MDs-MacBook-Air.local>
2025-06-10 20:43:18 +0600

    computed property

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,11 @@
 <script setup>
-// compute property
+// computed property
 import { ref, computed } from 'vue'
-
-const firstName = ref("John");
-const lastName = ref("Doe");
-
-const getFullName = () => {
-  console.log('Function Called');
-  return `${firstName.value} ${lastName.value}`;
-}
-
-const computedFullName = computed(() => {
-  console.log('Computed Called');
-  return `${firstName.value} ${lastName.value}`;
-});
-
-function changeName() {
-  console.log('Changing Name')
-  firstName.value = "Rahim";
-  lastName.value = "Uddin";
-}
 
 </script>
 
 <template>
   <header>
-    <p class="wrapper">
-      Your name is <em> {{ firstName }} {{ lastName }}</em><br>
-      <button @click="changeName()">Change Name</button>
-    </p>
-    <p>My name is {{ getFullName() }}</p>
-    <p> Your Full name is = {{ getFullName() }} </p></br>
-    <p> Your Full name is = {{ getFullName() }} </p></br>
-    <p> Your Full name is = {{ getFullName() }} </p></br>
-    <p>Your computed Full name is = {{ computedFullName }}</p></br>
-    <p>Your computed Full name is = {{ computedFullName }}</p></br>
-    <p>Your computed Full name is = {{ computedFullName }}</p></br>
-    <p>Your computed Full name is = {{ computedFullName }}</p></br>
 
   </header>
 
---
+++ src/Computed-2.vue
@@ -0,0 +1,41 @@
+<script setup>
+// computed property
+import { ref, computed } from 'vue'
+
+const number = ref(4);
+const doubleNumber = computed(() => {
+  return number.value * 2;
+})
+
+const squareNumber = computed(() => {
+  return number.value * number.value;
+})
+const cubeNumber = computed(() => {
+  return number.value * number.value * number.value;
+})
+const quadNumber = computed(() => {
+  return number.value * number.value * number.value * number.value;
+})
+
+function changeNumber() {
+  console.log("Changing number")
+  number.value = 5;
+}
+</script>
+
+<template>
+  <header>
+    <button @click="changeNumber()">Change Number</button>
+    <p>This is a number {{ number }}</p>
+    <p>This is double Double number = {{ doubleNumber }}</p>
+    <p>This is double Square number = {{ squareNumber }}</p>
+    <p>This is double Cube number = {{ cubeNumber }}</p>
+    <p>This is double Quad number = {{ quadNumber }}</p>
+  </header>
+
+  <main>
+    .
+  </main>
+</template>
+
+<style scoped></style>
