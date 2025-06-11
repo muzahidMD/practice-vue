905c9258428ae2e5a780f6b4b88f5eb2e0ba598c
MD Muzahidul Islam <muzahidul@MDs-MacBook-Air.local>
2025-06-11 21:13:57 +0600

    two way data binding with ref and reactive

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,37 @@
 <script setup>
-// computed property
-import { ref, computed } from 'vue'
+// Tow way data binding in input with reactive function
+import { reactive, ref } from 'vue';
+const person = reactive({
+  firstName: "John",
+  lastName: "Doe"
+})
 
+function changeName() {
+  console.log("Changing Name")
+  person.firstName = "Rahim";
+  person.lastName = "Uddin"
+}
 </script>
 
 <template>
   <header>
-
+    <p>
+      His name is {{ person.firstName }} <br>
+      His name is {{ person.lastName }} <br>
+    </p>
+    <p>
+      {{ person }}
+    </p>
+    <p>
+      First name: <input type="text" v-model="person.firstName">
+    </p>
+    <p>
+      Last name: <input type="text" v-model="person.lastName">
+    </p>
   </header>
 
   <main>
-    .
+    <button @click="changeName()">Change Name</button>
   </main>
 </template>
 
---
+++ src/InputDataBinding-ref.vue
@@ -0,0 +1,35 @@
+<script setup>
+// Tow way data binding in input with ref function
+import { ref } from 'vue';
+const firstName = ref("John");
+const lastName = ref("Doe");
+
+function changeName() {
+  console.log("Changing Name")
+  firstName.value = "Rahim";
+  lastName.value = "Uddin"
+}
+</script>
+
+<template>
+  <header>
+    <p>
+      His name is {{ firstName }} <br>
+      His name is {{ lastName }} <br>
+    </p>
+    <p>
+      First name: <input type="text" v-model="firstName">
+      <!-- First name: <input type="text" :value="firstName">  -->
+    </p>
+    <p>
+      Last name: <input type="text" v-model="lastName">
+      <!-- Last name: <input type="text" :value="lastName"> -->
+    </p>
+  </header>
+
+  <main>
+    <button @click="changeName()">Change Name</button>
+  </main>
+</template>
+
+<style scoped></style>
