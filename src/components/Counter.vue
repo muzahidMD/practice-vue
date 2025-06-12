fd9c972fca0cb4fa03015c8ac0b775c10ad57219
MD Muzahidul Islam <muzahidul@MDs-MacBook-Air.local>
2025-06-12 10:30:53 +0600

    data binding with component

---
+++ src/components/Counter.vue
@@ -0,0 +1,15 @@
+<script setup>
+const model = defineModel(); 
+</script>
+
+<template>
+    <hr>
+    <p>Counter Component</p>
+    <p>Count : {{ model }}</p>
+    <button @click="model++">Increment</button>
+    <button @click="model--">Decrement</button>
+    <button @click="model = 0">Reset</button>
+    <input type="number" v-model="model">
+</template>
+
+<style scoped></style>
---
+++ src/components/PersonComponent.vue
@@ -0,0 +1,26 @@
+<script setup>
+// const props = defineProps([
+//     'fn',
+//     'ln'
+// ]);
+const fn = defineModel('fn');
+const ln = defineModel('ln');
+
+function changingName() {
+    console.log("Change Name");
+    fn.value = "Rahim";
+    ln.value = "Uddin";
+}
+</script>
+
+<template>
+    <hr>
+    <p>Person Component</p>
+    <p>First Name: {{ fn }}</p>
+    <p>Last Name: {{ ln }}</p>
+    <button @click="changingName()">Change Name</button><br>
+    <p>First Name: <input type="text" v-model="fn"><br></p>
+    <p>Last Name:<input type="text" v-model="ln"><br></p>
+</template>
+
+<style scoped></style>
